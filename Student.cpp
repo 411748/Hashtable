6d830f2a7ce3cf570ff348ea451174f43ae97605
411748 <411748@bsd48.org>
2024-02-11 14:15:09 -0800

    added previous files

---
+++ Student.cpp
@@ -0,0 +1,34 @@
+#include "Student.h"
+
+//Building student
+student::student(const char* firstName, const char* lastName, int studentID, double GPA) {
+    this->firstName = new char[strlen(firstName) + 1];
+    strcpy(this->firstName, firstName);
+
+    this->lastName = new char[strlen(lastName) + 1];
+    strcpy(this->lastName, lastName);
+
+    this->studentID = studentID;
+    this->GPA = GPA;
+}
+//Deleting student
+student::~student() {
+    delete[] firstName;
+    delete[] lastName;
+}
+//Functions to build student
+const char* student::getFirstName() {
+    return firstName;
+}
+
+const char* student::getLastName() {
+    return lastName;
+}
+
+int student::getStudentID() {
+    return studentID;
+}
+
+double student::getGPA() {
+    return GPA;
+}
---
+++ Student.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <cstring>
+//Class for building students functions
+class student {
+public:
+    student(const char* firstName, const char* lastName, int studentID, double GPA);
+    ~student();
+    
+    const char* getFirstName();
+    const char* getLastName();
+    int getStudentID();
+    double getGPA();
+
+private:
+    char* firstName;
+    char* lastName;
+    int studentID;
+    double GPA;
+};
+
+#endif
